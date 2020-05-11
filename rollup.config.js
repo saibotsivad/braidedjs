381bcf6a1a2976239c809c3235fd3d4023e4f102
Tobias Davis <accounts@davistobias.com>
2020-05-11 09:03:57 -0500

    change: examples; ws implementation; use aws sdk in lambda

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import { string } from 'rollup-plugin-string'
+import builtins from 'builtin-modules'
 import md from 'rollup-plugin-md'
 import resolve from '@rollup/plugin-node-resolve'
 
-const plugins = [
+const browserPlugins = [
 	string({
 		include: [ '**/*.html' ]
 	}),
@@ -12,45 +13,57 @@ const plugins = [
 	md()
 ]
 
+const lambdaPlugins = [
+	string({
+		include: [ '**/*.html' ]
+	}),
+	resolve({
+		browser: false,
+		preferBuiltins: true,
+	}),
+	md()
+]
+
 export default [
-	{
-		input: './app/node_modules/api-rest/index.js',
-		output: {
-			format: 'esm',
-			file: './service/api-rest/index.js'
-		},
-		plugins
-	},
+	// {
+	// 	input: './app/node_modules/api-rest/index.js',
+	// 	output: {
+	// 		format: 'esm',
+	// 		file: './service/api-rest/index.js'
+	// 	},
+	// 	plugins: browserPlugins
+	// },
 	{
 		input: './app/node_modules/api-ws/index.js',
 		output: {
-			format: 'esm',
+			format: 'cjs',
 			file: './service/api-ws/index.js'
 		},
-		plugins
+		plugins: lambdaPlugins,
+		external: [ ...builtins, 'aws-sdk/clients/dynamodb' ]
 	},
-	{
-		input: './app/node_modules/stream-ws/index.js',
-		output: {
-			format: 'esm',
-			file: './service/stream-ws/index.js'
-		},
-		plugins
-	},
-	{
-		input: './app/node_modules/www-redirect/index.js',
-		output: {
-			format: 'esm',
-			file: './service/www-redirect/index.js'
-		},
-		plugins
-	},
-	{
-		input: './app/node_modules/www-root/index.js',
-		output: {
-			format: 'esm',
-			file: './service/www-root/index.js'
-		},
-		plugins
-	},
+	// {
+	// 	input: './app/node_modules/stream-ws/index.js',
+	// 	output: {
+	// 		format: 'esm',
+	// 		file: './service/stream-ws/index.js'
+	// 	},
+	// 	plugins: browserPlugins
+	// },
+	// {
+	// 	input: './app/node_modules/www-redirect/index.js',
+	// 	output: {
+	// 		format: 'esm',
+	// 		file: './service/www-redirect/index.js'
+	// 	},
+	// 	plugins: browserPlugins
+	// },
+	// {
+	// 	input: './app/node_modules/www-root/index.js',
+	// 	output: {
+	// 		format: 'esm',
+	// 		file: './service/www-root/index.js'
+	// 	},
+	// 	plugins: browserPlugins
+	// },
 ]
